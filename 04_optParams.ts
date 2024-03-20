a93bcdc3ff6ecb5e5408ff0166ee737cf2ddc54b
Mario Lazzari <mario.lazzari@gmail.com>
2024-03-20 22:42:58 +0100

    Done

--- 04_optParams.ts
+++
@@ -1,6 +0,0 @@
-const getName = (first: string, last?: string): string => {
-  if (last) {
-    return `${first} ${last}`;
-  }
-  return first;
-};
--- 16_select.ts
+++
@@ -1,9 +0,0 @@
-// https://www.typescriptlang.org/docs/handbook/utility-types.html
-interface User {
-  id: string;
-  firstName: string;
-  lastName: string;
-}
-
-type MyTypeOmit = Omit<User, "id">;
-type MyTypePick = Pick<User, "firstName" | "lastName">;
